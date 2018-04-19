5f24a02d2f9b46b0394fad4aaf99bd89c1b2c71e
Lewis Goddard <lewisgoddard@eustasy.org>
2018-04-19 20:36:16 +0100

    Update smooth-scrolling.js

--- assets/js/smooth-scrolling.js
+++ assets/js/smooth-scrolling.js
@@ -1,6 +1,7 @@
 ////	Smooth Scrolling
 // Scroll smoothly when an anchor is clicked,
 // or the location.hash changes.
+/*eslint-env jquery*/
 $(function () {
 	function ScrollToHash(hash) {
 		var $hash = $(hash);
@@ -8,20 +9,20 @@ $(function () {
 			$('html, body').animate({
 				scrollTop: $hash.offset().top
 			}, 500, function () {
-				window.location.hash = hash;
-			});
+				window.location.hash = hash
+			})
 		}
 	}
 	// Scroll on click.
 	$('a').click(function() {
-		var hash = $.attr(this, 'href');
-		ScrollToHash(hash);
-		return false;
-	});
+		var hash = $.attr(this, 'href')
+		ScrollToHash(hash)
+		return false
+	})
 	// Scroll for back or forward buttons.
 	window.onhashchange = ( function(){
-		ScrollToHash(window.location.hash);
-	});
+		ScrollToHash(window.location.hash)
+	})
 	// Trigger on page load.
-	ScrollToHash(window.location.hash);
+	ScrollToHash(window.location.hash)
 });
